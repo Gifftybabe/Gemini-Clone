aee0f0976a159aba2d59ca33c2fb654b8fa837f8
Gifftybabe <ulokangozi@gmail.com>
2024-06-06 15:05:34 +0100

    Updated the UI with gemini prompts

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,11 +3,16 @@ import "./Main.css";
 import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
 
-
 const Main = () => {
-
-  const { onSent, recentPrompt, showResult, loading, resultData, setInput, input } = useContext(Context)
-
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
 
   return (
     <div className="main">
@@ -16,34 +21,62 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
-        <div className="greet">
-          <p>
-            <span>Hello, Dev.</span>
-          </p>
-          <p>How can I help you today?</p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>Recipe with whats in my Kitchen</p>
-            <img src={assets.compass_icon} alt="" />
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Dev.</span>
+              </p>
+              <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Recipe with whats in my Kitchen</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Morning routine for productivity</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Email for plumber quote </p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Python script for daily business reports </p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading
+              ?<div className="loader">
+                <hr />
+                <hr />
+                <hr />
+              </div>
+              :<p dangerouslySetInnerHTML={{__html:resultData}}></p>
+            }
+              
+            </div>
           </div>
-          <div className="card">
-            <p>Morning routine for productivity</p>
-            <img src={assets.bulb_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Email for plumber quote </p>
-            <img src={assets.message_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Python script for daily business reports </p>
-            <img src={assets.code_icon} alt="" />
-          </div>
-        </div>
+        )}
 
         <div className="main-bottom">
           <div className="search-box">
-            <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder="Enter prompt here" />
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter prompt here"
+            />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -5,21 +5,47 @@ import { ChatSession } from "@google/generative-ai";
 export const Context = createContext();
 
 const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
 
-  const [input, setInput] = useState('')
-  const [recentPrompt, setRecentPrompt] = useState('')
-  const [prevPrompts, setPrevPrompts] = useState([])
-  const [showResult, setShowResult ] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const [resultData, setResultData] = useState('')
-
-
-
-  const onSent = async (prompt) => {
-    await run(input);
+  const delayPara = (index, nextWord) => {
+    setTimeout(function () {
+      setResultData(prev => prev + nextWord)
+    }, 75*index)
+    
   };
 
+  const onSent = async (prompt) => {
 
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input);
+    setPrevPrompts(prev => [...prev, input])
+
+    const response = await run(input);
+    let responseArray = response.split("**");
+    let newResponse;
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+    let newresponse2 = newResponse.split("*").join("</br>");
+   let newResponseArray = newresponse2.split('')
+   for(let i = 0; i < newResponseArray.length; i++) {
+    const nextWord = newResponseArray[i]
+    delayPara(i, nextWord + '')
+   }
+    setLoading(false);
+    setInput("");
+  };
 
   const contextValue = {
     prevPrompts,
@@ -31,7 +57,7 @@ const ContextProvider = (props) => {
     loading,
     resultData,
     input,
-    setInput
+    setInput,
   };
 
   return (
